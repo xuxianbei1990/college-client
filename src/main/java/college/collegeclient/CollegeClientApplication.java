ca672038313e42e19b2296948706f35a5c8649f5
xuxianbei1990 <34258324+xuxianbei1990@users.noreply.github.com>
2019-04-19 19:48:49 +0800

    Initial commit

---
+++ src/main/java/college/collegeclient/CollegeClientApplication.java
@@ -0,0 +1,13 @@
+package college.collegeclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/college/collegeclient/CollegeClientApplicationTests.java
@@ -0,0 +1,16 @@
+package college.collegeclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CollegeClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
